1c07a82ef95996278f64cf6b6b0b6d8d5a98093e
mikshepelev <m.shepelev@sportradar.com>
2022-11-03 17:17:30 +0200

    Restructure maven dependencies
    Replace all dependencies related to junit 4 with new jupiter dependencies

---
+++ sdk-test/src/test/java/com/sportradar/livedata/sdk/common/exceptions/InvalidPropertyExceptionTest.java
@@ -0,0 +1,12 @@
+package com.sportradar.livedata.sdk.common.exceptions;
+
+
+import org.junit.jupiter.api.Test;
+
+public class InvalidPropertyExceptionTest {
+
+    @Test
+    public void constructorTest() {
+
+    }
+}
